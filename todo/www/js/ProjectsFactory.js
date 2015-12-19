ac42459945be265403010da6f86c85c73c0f77eb
Daniel Berrocal <jdanielb1992@gmail.com>
2015-12-19 16:48:37 -0600

    Todo App

---
+++ todo/www/js/ProjectsFactory.js
@@ -0,0 +1,31 @@
+/**
+ * Created by j-dan on 19/12/2015.
+ */
+angular.module('todo')
+  .factory('Projects', function() {
+    return {
+      all: function() {
+        var projectString = window.localStorage['projects'];
+        if(projectString) {
+          return angular.fromJson(projectString);
+        }
+        return [];
+      },
+      save: function(projects) {
+        window.localStorage['projects'] = angular.toJson(projects);
+      },
+      newProject: function(projectTitle) {
+        // Add a new project
+        return {
+          title: projectTitle,
+          tasks: []
+        };
+      },
+      getLastActiveIndex: function() {
+        return parseInt(window.localStorage['lastActiveProject']) || 0;
+      },
+      setLastActiveIndex: function(index) {
+        window.localStorage['lastActiveProject'] = index;
+      }
+    }
+  })
